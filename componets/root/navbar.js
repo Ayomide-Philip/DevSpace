67821726f10e745ec57c4f6c3fb84c9a5ce3497f
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-25 22:06:58 +0100

    updating my codebase

--- componets/root/navbar.js
+++ componets/root/navbar.js
@@ -9,51 +9,31 @@ import {
 import Link from "next/link";
 
 export default function NavBar() {
+  const navbarLink = [
+    { path: "/", name: "Home", icon: HomeIcon },
+    { path: "/about", name: "About", icon: Info },
+    { path: "/projects", name: "Projects", icon: Grid },
+    { path: "/team", name: "Teams", icon: Users2 },
+    { path: "/contact", name: "Contact", icon: Contact2 },
+    { path: "/blog", name: "Blog", icon: MessageCircle },
+  ];
   return (
-    <div className="dark:bg-gray-800 bg-gray-100 border-gray-600 border fixed bottom-2 left-1/2 transform -translate-1/2 flex rounded-2xl">
+    <div className="dark:bg-gray-800 bg-gray-100 border-gray-600 border fixed bottom-1 left-1/2 transform -translate-1/2 flex rounded-2xl">
       <div className="flex gap-2 dark:text-white">
-        <Link
-          className="dark:text-white transition py-2 px-2 hover:bg-gray-700 flex flex-col justify-center items-center rounded-l-2xl"
-          href={"#"}
-        >
-          <HomeIcon className="h-5 w-5" />
-          <span className="text-sm">Home</span>
-        </Link>
-        <Link
-          className="dark:text-white transition py-2 px-2 hover:bg-gray-700 flex flex-col justify-center items-center"
-          href={"#"}
-        >
-          <Info className="h-5 w-5" />
-          <span className="text-sm">About</span>
-        </Link>
-        <Link
-          className="dark:text-white transition py-2 px-2 hover:bg-gray-700 flex flex-col justify-center items-center"
-          href={"#"}
-        >
-          <Grid className="h-5 w-5" />
-          <span className="text-sm">Projects</span>
-        </Link>
-        <Link
-          className="dark:text-white transition py-2 px-2 hover:bg-gray-700 flex flex-col justify-center items-center"
-          href={"#"}
-        >
-          <Users2 className="h-5 w-5" />
-          <span className="text-sm">Team</span>
-        </Link>
-        <Link
-          className="dark:text-white transition py-2 px-2 hover:bg-gray-700 flex flex-col justify-center items-center"
-          href={"#"}
-        >
-          <Contact2 className="h-5 w-5" />
-          <span className="text-sm">Contact</span>
-        </Link>
-        <Link
-          className="dark:text-white transition py-2 px-2 hover:bg-gray-700 flex flex-col justify-center items-center"
-          href={"#"}
-        >
-          <MessageCircle className="h-5 w-5" />
-          <span className="text-sm">Blog</span>
-        </Link>
+        {navbarLink.map(({ path, name, icon: Icon }, idx) => {
+          return (
+            <Link
+              key={idx}
+              className={`dark:text-white transition py-3 px-3 hover:bg-gray-300 dark:hover:bg-gray-700 flex flex-col justify-center items-center ${
+                idx === 0 && " rounded-l-2xl"
+              } ${idx === navbarLink.length - 1 && " rounded-r-2xl"}`}
+              href={path}
+            >
+              <Icon className="h-5 w-5" />
+              <span className="text-sm">{name}</span>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
