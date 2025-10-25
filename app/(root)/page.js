d53f277a9bd1c4f7051d5dc38ab7a440d388efcc
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-25 22:52:19 +0100

    updating my codebase

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,9 +1,13 @@
+import AboutSection from "@/componets/root/home/aboutsection";
 import HeroSection from "@/componets/root/home/herosection";
+import WhatWeDo from "@/componets/root/home/whatwedosection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <AboutSection />
+      <WhatWeDo />
     </>
   );
 }
