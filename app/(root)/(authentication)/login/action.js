a9109c27e31929409af5799c96bbf33ad519d553
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-27 13:10:14 +0100

    removing uncessary log

--- app/(root)/(authentication)/login/action.js
+++ app/(root)/(authentication)/login/action.js
@@ -3,7 +3,6 @@ import { signIn } from "next-auth/react";
 export async function LoginAction(formData) {
   const email = formData.get("emailAddress");
   const password = formData.get("password");
-  console.log(email, password);
   const res = await signIn("credentials", {
     email,
     password,
