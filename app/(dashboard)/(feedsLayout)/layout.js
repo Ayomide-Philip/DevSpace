a3cb3f27fc0833dbb2f2cfc89156e8cbc55259f0
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-31 14:25:28 +0100

    updating my codebase

--- app/(dashboard)/(feedsLayout)/layout.js
+++ app/(dashboard)/(feedsLayout)/layout.js
@@ -8,7 +8,7 @@ export default async function Layout({ children }) {
     <>
       <div className="grid grid-cols-1 lg:grid-cols-[22%_50%_22%] justify-center dark:bg-gray-900 dark:text-white gap-2">
         <LeftSideBar />
-        <div className="flex flex-col mt-3 gap-4 justify-center items-center mb-15 md:mb-25">
+        <div className="flex flex-col mt-3 gap-4 justify-center items-center mb-15 md:mb-30">
           {children}
         </div>
         <RightSidebar />
